74bd9d5be857f998599e9599bb0f7bd1e2d00793
Sukhnandan Singh <sukhnandan.singh@mail.polimi.it>
2023-03-24 16:04:46 +0100

    Card and position updates , and bag partial implementation

--- src/main/java/it/polimi/ingsw/Card.java
+++ src/main/java/it/polimi/ingsw/Card.java
@@ -5,9 +5,11 @@ public class Card {
     private String colour;
     private Position coordinates;
 
-    public Card(String colour , Position coordinates){
+    public Card(String colour){
         this.colour=colour;
-        this.coordinates=coordinates;
+    }
+    public void setCoordinates(Position coordinatesParametre){
+        coordinates=coordinatesParametre; //Coordinates to be set at the value passed as parametre in the method
     }
 
     public String getColour(){
