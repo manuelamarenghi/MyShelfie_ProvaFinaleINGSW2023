d36c80111e8c82752c56cf7d7d89bde9785ca0e7
Silvia_Liu <silviaruxin.liu@mail.polimi.it>
2023-03-27 11:23:53 +0200

    CommonGoalCards interface

---
+++ src/main/java/it/polimi/ingsw/CommonGoalCard/CommonGoalCards.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.CommonGoalCard;
+
+import it.polimi.ingsw.Library;
+
+public interface CommonGoalCards {
+    public boolean check(Library library);
+    //public boolean expiered(Player player);
+    public void getImage();
+
+}
