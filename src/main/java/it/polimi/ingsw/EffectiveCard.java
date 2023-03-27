11c1ccff90591931812f85b878a3fb5f2a86bc77
Silvia_Liu <silviaruxin.liu@mail.polimi.it>
2023-03-27 11:56:44 +0200

    Aggiornamento

--- src/main/java/it/polimi/ingsw/EffectiveCard.java
+++ src/main/java/it/polimi/ingsw/EffectiveCard.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.CommonGoalCard;
 
+import it.polimi.ingsw.CommonGoalCard.CommonGoalCards;
+
 public class EffectiveCard {
     private CommonGoalCards commonCard;
     private int[] allScores;
