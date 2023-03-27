965593747b4f7ee74dd22a2fe913648afbca923a
LucaLaPietra <111192833+LucaLaPietra@users.noreply.github.com>
2023-03-27 15:43:30 +0200

    Commit to get Player class i have to commit to get Player class from another branch, in order to test my EightEqualsCardsTest. (Errors are only due to Player missing class and some BoardTest part that i don't know the structure)

--- src/test/java/it/polimi/ingsw/EightEqualsCardsTest.java
+++ src/test/java/it/polimi/ingsw/EightEqualsCardsTest.java
@@ -1,7 +1,23 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.CommonGoalCard.EightEqualsCards;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EightEqualsCardsTest {
-
+    private CommonGoalCards eightEC=new EightEqualsCards();
+    @Test
+    void checkIfClassWorksProperly(){
+        Library library = new Library();
+        library.getCardinPos(5, 2).setColour("blue");
+        library.getCardinPos(4, 2).setColour("blue");
+        library.getCardinPos(3, 2).setColour("blue");
+        library.getCardinPos(2, 2).setColour("blue");
+        library.getCardinPos(1, 2).setColour("blue");
+        library.getCardinPos(5, 1).setColour("blue");
+        library.getCardinPos(3, 0).setColour("blue");
+        library.getCardinPos(4, 0).setColour("blue");
+        assertTrue(eightEC.check(library));
+    }
 }
