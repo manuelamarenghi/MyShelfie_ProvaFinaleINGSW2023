8398cb0d760270e9a878a58ad6af67444fab1832
manuela <manuelamarenghi@mail.polimi.it>
2023-03-27 09:49:41 +0200

    aggiunta library

--- src/main/java/it/polimi/ingsw/Card.java
+++ src/main/java/it/polimi/ingsw/Card.java
@@ -1,19 +1,35 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.Position;
+
 public class Card {
 
     private String colour;
     private Position coordinates;
-
-    public Cards(String colour , Position coordinates){
+    private int visited;
+    public Card(){
+        this.colour="";
+        this.coordinates=null;}
+    public Card(String colour,Position position){
+        this.coordinates=position;
         this.colour=colour;
-        this.coordinates=coordinates;
     }
 
     public String getColour(){
-        return colour;
-    }
+        return colour;}
+
     public Position getCoordinates(){
         return coordinates;
+    }
+    public void setCoordinates(Position coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setVisited(int visited) {
+        this.visited = visited;
     }
 }
