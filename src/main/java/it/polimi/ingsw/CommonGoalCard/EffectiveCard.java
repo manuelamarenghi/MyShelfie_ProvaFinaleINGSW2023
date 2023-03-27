fc74251ab477bd269ff9a5669b8ae9f9a4bd63b7
Sukhnandan Singh <sukhnandan.singh@mail.polimi.it>
2023-03-27 11:27:29 +0200

    Board class and BoardTest yet to finish

--- src/main/java/it/polimi/ingsw/CommonGoalCard/EffectiveCard.java
+++ src/main/java/it/polimi/ingsw/CommonGoalCard/EffectiveCard.java
@@ -1,7 +1,5 @@
 package it.polimi.ingsw.CommonGoalCard;
 
-import it.polimi.ingsw.CommonGoalCard.CommonGoalCards;
-
 public class EffectiveCard {
     private CommonGoalCards commonCard;
     private int[] allScores;
--- src/test/java/it/polimi/ingsw/CardTest.java
+++ src/test/java/it/polimi/ingsw/CardTest.java
@@ -7,14 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class CardTest{
     @Test
     public void getColorReturnsGreen(){
-        Card testcard = new Card("Green");
-        testcard.setCoordinates(new Position(2,4));
+        Card testcard = new Card("Green" , new Position(2,4));
         assertEquals("Green" , testcard.getColour());
     }
     @Test
     public void getPositionReturnsCoordinates(){
-        Card testcard = new Card("Green");
-        testcard.setCoordinates(new Position(2,4));
+        Card testcard = new Card("Green" , new Position(2,4));
         assertEquals(2 , testcard.getCoordinates().getX());
         assertEquals(4, testcard.getCoordinates().getY());
     }
