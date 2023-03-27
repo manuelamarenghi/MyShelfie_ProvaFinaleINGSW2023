11ae6a4d39acbd2400f238189b73179ac5589dce
Silvia_Liu <silviaruxin.liu@mail.polimi.it>
2023-03-27 11:30:15 +0200

    EffectiveCard bozza

--- src/main/java/it/polimi/ingsw/CommonGoalCard/EffectiveCard.java
+++ src/main/java/it/polimi/ingsw/CommonGoalCard/EffectiveCard.java
@@ -6,4 +6,15 @@ public class EffectiveCard {
     private CommonGoalCards commonCard;
     private int[] allScores;
 
+    public EffectiveCard(CommonGoalCards commoncard ){
+        this.commonCard = commoncard;
+    }
+
+    public void update(Player player){
+        commonCard.check(player.getLibrary());
+    }
+
+    public void show(){
+        commonCard.getImage();
+    }
 }
