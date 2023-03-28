4f345e3946882155c88096c0f342b46486b2e64d
LucaLaPietra <111192833+LucaLaPietra@users.noreply.github.com>
2023-03-28 10:48:13 +0200

    Finished test for StairsGoalCard:check() all 4 check:true cases and 2 check:false one i also added 2 methods in Library to get for Row/Column how many cards they contain.

--- src/test/java/it/polimi/ingsw/StairsGoalCardTest.java
+++ src/test/java/it/polimi/ingsw/StairsGoalCardTest.java
@@ -23,17 +23,15 @@ class StairsGoalCardTest {
         library.getCardinPos(4, 1).setColour("blue");
         library.getCardinPos(4, 2).setColour("blue");
         library.getCardinPos(4, 3).setColour("blue");
-
-
+        //third row
         library.getCardinPos(3, 0).setColour("blue");
         library.getCardinPos(3, 1).setColour("blue");
         library.getCardinPos(3, 2).setColour("blue");
-
+        //fourth row
         library.getCardinPos(2, 0).setColour("blue");
         library.getCardinPos(2, 1).setColour("blue");
-
+        //fifth row
         library.getCardinPos(1, 0).setColour("blue");
-        library.showLibrary();
 
         assertTrue(stair.check(library));
 
@@ -42,35 +40,170 @@ class StairsGoalCardTest {
     @Test
     void checkShiftedSxStair(){
         Library library = new Library();
-        //first column
+        //first row
         library.getCardinPos(5, 0).setColour("blue");
-        library.getCardinPos(4, 0).setColour("blue");
-        library.getCardinPos(3, 0).setColour("blue");
-        library.getCardinPos(2, 0).setColour("blue");
-        library.getCardinPos(1, 0).setColour("blue");
-        library.getCardinPos(0, 0).setColour("blue");
-        //second column
         library.getCardinPos(5, 1).setColour("blue");
-        library.getCardinPos(4, 1).setColour("blue");
-        library.getCardinPos(3, 1).setColour("blue");
-        library.getCardinPos(2, 1).setColour("blue");
-        library.getCardinPos(1, 1).setColour("blue");
-
-
         library.getCardinPos(5, 2).setColour("blue");
-        library.getCardinPos(4, 2).setColour("blue");
-        library.getCardinPos(3, 2).setColour("blue");
-        library.getCardinPos(2, 2).setColour("blue");
-
         library.getCardinPos(5, 3).setColour("blue");
-        library.getCardinPos(4, 3).setColour("blue");
-        library.getCardinPos(3, 3).setColour("blue");
-
         library.getCardinPos(5, 4).setColour("blue");
+        //second row
+        library.getCardinPos(4, 0).setColour("blue");
+        library.getCardinPos(4, 1).setColour("blue");
+        library.getCardinPos(4, 2).setColour("blue");
+        library.getCardinPos(4, 3).setColour("blue");
         library.getCardinPos(4, 4).setColour("blue");
+        //third row
+        library.getCardinPos(3, 0).setColour("blue");
+        library.getCardinPos(3, 1).setColour("blue");
+        library.getCardinPos(3, 2).setColour("blue");
+        library.getCardinPos(3, 3).setColour("blue");
+        //fourth row
+        library.getCardinPos(2, 0).setColour("blue");
+        library.getCardinPos(2, 1).setColour("blue");
+        library.getCardinPos(2, 2).setColour("blue");
+        //fifth row
+        library.getCardinPos(1, 0).setColour("blue");
+        library.getCardinPos(1, 1).setColour("blue");
+        //sixth row
+        library.getCardinPos(0, 0).setColour("blue");
 
         assertTrue(stair.check(library));
 
     }
+    @Test
+    void checkDxStair(){
+        Library library = new Library();
 
+        //first row
+        library.getCardinPos(5, 0).setColour("blue");
+        library.getCardinPos(5, 1).setColour("blue");
+        library.getCardinPos(5, 2).setColour("blue");
+        library.getCardinPos(5, 3).setColour("blue");
+        library.getCardinPos(5, 4).setColour("blue");
+        //second row
+        library.getCardinPos(4, 4).setColour("blue");
+        library.getCardinPos(4, 1).setColour("blue");
+        library.getCardinPos(4, 2).setColour("blue");
+        library.getCardinPos(4, 3).setColour("blue");
+        //third row
+        library.getCardinPos(3, 4).setColour("blue");
+        library.getCardinPos(3, 3).setColour("blue");
+        library.getCardinPos(3, 2).setColour("blue");
+        //fourth row
+        library.getCardinPos(2, 4).setColour("blue");
+        library.getCardinPos(2, 3).setColour("blue");
+        //fifth row
+        library.getCardinPos(1, 4).setColour("blue");
+
+        assertTrue(stair.check(library));
+
+
+    }
+    @Test
+    void checkShiftedDxStair(){
+        Library library = new Library();
+
+        //first row
+        library.getCardinPos(5, 0).setColour("blue");
+        library.getCardinPos(5, 1).setColour("blue");
+        library.getCardinPos(5, 2).setColour("blue");
+        library.getCardinPos(5, 3).setColour("blue");
+        library.getCardinPos(5, 4).setColour("blue");
+        //second row
+        library.getCardinPos(4, 4).setColour("blue");
+        library.getCardinPos(4, 1).setColour("blue");
+        library.getCardinPos(4, 2).setColour("blue");
+        library.getCardinPos(4, 3).setColour("blue");
+        library.getCardinPos(4, 0).setColour("blue");
+        //third row
+        library.getCardinPos(3, 4).setColour("blue");
+        library.getCardinPos(3, 3).setColour("blue");
+        library.getCardinPos(3, 2).setColour("blue");
+        library.getCardinPos(3, 1).setColour("blue");
+
+        //fourth row
+        library.getCardinPos(2, 4).setColour("blue");
+        library.getCardinPos(2, 3).setColour("blue");
+        library.getCardinPos(2, 2).setColour("blue");
+
+        //fifth row
+        library.getCardinPos(1, 4).setColour("blue");
+        library.getCardinPos(1, 3).setColour("blue");
+
+        library.getCardinPos(0, 4).setColour("blue");
+
+        assertTrue(stair.check(library));
+
+
+    }
+    @Test
+    void checkWrongCase1(){
+        Library library = new Library();
+
+        //first row
+        library.getCardinPos(5, 0).setColour("blue");
+        library.getCardinPos(5, 1).setColour("blue");
+        library.getCardinPos(5, 2).setColour("blue");
+        library.getCardinPos(5, 3).setColour("blue");
+        library.getCardinPos(5, 4).setColour("blue");
+        //second row
+        library.getCardinPos(4, 4).setColour("blue");
+        library.getCardinPos(4, 1).setColour("blue");
+        library.getCardinPos(4, 2).setColour("blue");
+        library.getCardinPos(4, 3).setColour("blue");
+        library.getCardinPos(4, 0).setColour("blue");
+        //third row
+        library.getCardinPos(3, 4).setColour("blue");
+        library.getCardinPos(3, 3).setColour("blue");
+        library.getCardinPos(3, 2).setColour("blue");
+        library.getCardinPos(3, 1).setColour("blue");
+        library.getCardinPos(3, 0).setColour("blue");
+
+        //fourth row
+        library.getCardinPos(2, 4).setColour("blue");
+        library.getCardinPos(2, 3).setColour("blue");
+        library.getCardinPos(2, 2).setColour("blue");
+
+        //fifth row
+        library.getCardinPos(1, 4).setColour("blue");
+        library.getCardinPos(1, 3).setColour("blue");
+
+        library.getCardinPos(0, 4).setColour("blue");
+
+        assertFalse(stair.check(library));
+
+
+    }
+    @Test
+    void checkWrongCase2(){
+        Library library = new Library();
+
+        //first row
+        library.getCardinPos(5, 0).setColour("blue");
+        library.getCardinPos(5, 1).setColour("blue");
+        library.getCardinPos(5, 2).setColour("blue");
+        library.getCardinPos(5, 3).setColour("blue");
+        library.getCardinPos(5, 4).setColour("blue");
+        //third row
+        library.getCardinPos(3, 4).setColour("blue");
+        library.getCardinPos(3, 3).setColour("blue");
+        library.getCardinPos(3, 2).setColour("blue");
+        library.getCardinPos(3, 1).setColour("blue");
+        library.getCardinPos(3, 0).setColour("blue");
+
+        //fourth row
+        library.getCardinPos(2, 4).setColour("blue");
+        library.getCardinPos(2, 3).setColour("blue");
+        library.getCardinPos(2, 2).setColour("blue");
+
+        //fifth row
+        library.getCardinPos(1, 4).setColour("blue");
+        library.getCardinPos(1, 3).setColour("blue");
+
+        library.getCardinPos(0, 4).setColour("blue");
+
+        assertFalse(stair.check(library));
+
+
+    }
 }
