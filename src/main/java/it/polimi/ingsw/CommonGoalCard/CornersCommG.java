81ad945a1c6061383b49dfb5966ad92629d499b3
Silvia_Liu <silviaruxin.liu@mail.polimi.it>
2023-03-30 16:41:21 +0200

    CommonCard Diagonal5 and Test

--- src/main/java/it/polimi/ingsw/CommonGoalCard/CornersCommG.java
+++ src/main/java/it/polimi/ingsw/CommonGoalCard/CornersCommG.java
@@ -9,16 +9,16 @@ import java.util.ArrayList;
 public class CornersCommG implements CommonGoalCards{
 
     public boolean check(Library library){
-        Card carta00 = new Card(library.getCardinPos(0,0).getColour(),library.getCardinPos(0,0).getCoordinates());
-        Card carta05 = new Card(library.getCardinPos(0,4).getColour(),library.getCardinPos(0,4).getCoordinates());
-        Card carta60 = new Card(library.getCardinPos(5,0).getColour(),library.getCardinPos(5,0).getCoordinates());
-        Card carta65 = new Card(library.getCardinPos(5,4).getColour(),library.getCardinPos(5,4).getCoordinates());
+
+        Card card00 = new Card(library.getCardinPos(0,0).getColour(),library.getCardinPos(0,0).getCoordinates());
+        Card card04 = new Card(library.getCardinPos(0,4).getColour(),library.getCardinPos(0,4).getCoordinates());
+        Card card50 = new Card(library.getCardinPos(5,0).getColour(),library.getCardinPos(5,0).getCoordinates());
+        Card card54 = new Card(library.getCardinPos(5,4).getColour(),library.getCardinPos(5,4).getCoordinates());
 
 
-
-        if(carta00.getColour() != "" && carta05.getColour() != "" && carta60.getColour() != "" && carta65.getColour() != ""){
-            if(carta00.getColour() == carta05.getColour() && carta05.getColour() == carta60.getColour()
-                    && carta60.getColour() == carta65.getColour())
+        if(card00.getColour() != "" && card04.getColour() != "" && card50.getColour() != "" && card54.getColour() != ""){
+            if(card00.getColour() == card04.getColour() && card04.getColour() == card50.getColour()
+                    && card50.getColour() == card54.getColour())
                 return true;
             else return false;
         }
@@ -30,19 +30,19 @@ public class CornersCommG implements CommonGoalCards{
 
     public boolean expired(Library library){
         boolean expire = false;
-        ArrayList<Card> carte = new ArrayList<Card>();
+        ArrayList<Card> cards = new ArrayList<Card>();
         if(library.getCardinPos(0,0).getColour()!="")
-            carte.add(new Card(library.getCardinPos(0,0).getColour(),library.getCardinPos(0,0).getCoordinates()));
+            cards.add(new Card(library.getCardinPos(0,0).getColour(),library.getCardinPos(0,0).getCoordinates()));
         if(library.getCardinPos(0,4).getColour()!="")
-            carte.add(new Card(library.getCardinPos(0,4).getColour(),library.getCardinPos(0,4).getCoordinates()));
+            cards.add(new Card(library.getCardinPos(0,4).getColour(),library.getCardinPos(0,4).getCoordinates()));
         if(library.getCardinPos(5,0).getColour()!="")
-            carte.add(new Card(library.getCardinPos(5,0).getColour(),library.getCardinPos(5,0).getCoordinates()));
+            cards.add(new Card(library.getCardinPos(5,0).getColour(),library.getCardinPos(5,0).getCoordinates()));
         if(library.getCardinPos(5,4).getColour()!="")
-            carte.add(new Card(library.getCardinPos(5,4).getColour(),library.getCardinPos(5,4).getCoordinates()));
+            cards.add(new Card(library.getCardinPos(5,4).getColour(),library.getCardinPos(5,4).getCoordinates()));
 
-        String colore = carte.get(0).getColour();
+        String colore = cards.get(0).getColour();
 
-        for ( Card c: carte){
+        for ( Card c: cards){
             if(c.getColour().equals(colore) == false){
                 expire = true;
             }
