3180762c3577d7d21d09f1b77e5fc8b67d884716
LucaLaPietra <111192833+LucaLaPietra@users.noreply.github.com>
2023-04-03 16:19:03 +0200

    Fixed EffectiveCard:getAllScore+EffectiveCardTest

---
+++ src/test/java/it/polimi/ingsw/EffectiveCardTest.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.CommonGoalCard.XGoalCard;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EffectiveCardTest {
+@Test
+    void checkIfWork(){
+    CommonGoalCards cgc=new XGoalCard();
+    EffectiveCard effectiveCard=new EffectiveCard(cgc);
+    ArrayList<Integer> al=new ArrayList<>();
+    al.add(1);
+    al.add(1);
+    al.add(1);
+    al.add(1);
+    al.add(1);
+    effectiveCard.setAllScores(al);
+    al=effectiveCard.getAllScores();
+    al.add(1);
+    assertNotSame(effectiveCard.getAllScores(), al);
+  }
+}
