7ec267fb1c66e2a5d6c48848622e90b0962926fc
manuela <manuelamarenghi@mail.polimi.it>
2023-04-13 00:00:45 +0200

    inizio networking

--- src/main/java/it/polimi/ingsw/network/ClientHandler.java
+++ src/main/java/it/polimi/ingsw/network/ClientHandler.java
@@ -7,6 +7,7 @@ import java.util.Map;
  */
 public class ClientHandler implements Runnable{
 
+
     @Override
     public void run() {
 
--- src/main/java/it/polimi/ingsw/network/SocketServer.java
+++ src/main/java/it/polimi/ingsw/network/SocketServer.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.network;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SocketServer implements Runnable{
     private final Server server;
@@ -12,13 +13,28 @@ public class SocketServer implements Runnable{
         this.server = server;
         this.port=port;
     }
-
+    /**
+     * run() open connection from the server and accept the client's socket creating its ClientHandler that will manage its request
+     */
     @Override
     public void run() {
         try {
             serverSocket = new ServerSocket(port);
         } catch (IOException e) {
 
+        }
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Socket client = serverSocket.accept();
+
+                client.setSoTimeout(5000);
+
+                ClientHandler clientHandler = new ClientHandler();
+                Thread thread = new Thread(clientHandler, "ss_handler" + client.getInetAddress());
+                thread.start();
+            } catch (IOException e) {
+
+            }
         }
     }
 }
