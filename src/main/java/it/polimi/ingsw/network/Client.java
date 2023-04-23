c186f49e8c8506730391f3cbdbe34e8fd4285473
LucaLaPietra <111192833+LucaLaPietra@users.noreply.github.com>
2023-04-23 14:39:02 +0200

    Modified Client.java and created SocketClient

--- src/main/java/it/polimi/ingsw/network/Client.java
+++ src/main/java/it/polimi/ingsw/network/Client.java
@@ -1,9 +1,17 @@
 package it.polimi.ingsw.network;
 
-public class Client {
-    private final int IDClient;
+import it.polimi.ingsw.message.Message;
 
-    public Client(int idClient) {
+import java.util.Observable;
+
+public abstract class Client extends Observable {
+    //private final int IDClient;
+    /*public Client(int idClient) {
         IDClient = idClient;
-    }
+    }*/
+    public abstract void sendMessage(Message message);
+
+    public abstract void readMessage();
+
+    public abstract void disconnect();
 }
