d0103a3f7bc9778ec1d49b2b9a0c85a32fb01f85
LiuSilvia2001 <112330331+LiuSilvia2001@users.noreply.github.com>
2023-05-02 14:57:02 +0200

    Ruxin controller (#86)
    * Controllo singol card nella plancia
    
    * check board empty
    
    * controller match
    
    * match controller
    
    * match controller
    
    * match controller
    
    * match controller
    
    * match controller
    
    * match controller: put in library
    
    * Update Show_Ccard.java
    
    * Update Show_Pcard.java
    
    * next turn
    
    * end game, first finish
    
    * aggiorno visit in message
    
    * matchController Test
    
    * library is full
    
    * Update Match.java

--- src/main/java/it/polimi/ingsw/Controller/MatchController.java
+++ src/main/java/it/polimi/ingsw/Controller/MatchController.java
@@ -27,7 +27,9 @@ public class MatchController {
     private ArrayList<String> disconnectClients;
     private Map<String, VirtualView> connectClients;
 
-    private boolean isStarted;
+
+    private boolean isStarted = false;
+
 
     private GameState gameState;
     private TurnPhase turnPhase;
@@ -48,6 +50,9 @@ public class MatchController {
         return isStarted;
     }
 
+    public Match getMatch(){return match;}
+
+
     /**
      * Set the State of the Game.
      *
@@ -55,6 +60,10 @@ public class MatchController {
      */
     private void setGameState(GameState gameState) {
         this.gameState = gameState;
+    }
+
+    private void addPlayers(String nickname){
+        players.add(nickname);
     }
 
 
@@ -66,16 +75,17 @@ public class MatchController {
     public void loginHandler(String nickname,VirtualView virtualView){
         if(connectClients.isEmpty()){
             addVirtualView(nickname,virtualView);
-            match.setPlayers(new Player(nickname));
+            addPlayers(nickname);
 
             //chiamata virtualview
 
         }
         else if(connectClients.size() < match.getPlayerNumber()){
             addVirtualView(nickname,virtualView);
-            match.setPlayers(new Player(nickname));
+            addPlayers(nickname);
         }
         else{
+            startGame();
             //avviso virtualview che ci sono già abbastanza giocatori
 
         }
@@ -89,13 +99,57 @@ public class MatchController {
             this.match.setPlayers(new Player(name));
         }
 
-        match.getMatchmanager().startGame();
 
+        match.getMatchmanager().startGame(match);
 
-        this.turnController = new TurnController(this,players,match.getChair().getNickname(),match);
-        turnController.setActivePlayer(match.getChair().getNickname());
+        String firstPlayer = match.getChair().getNickname();
+        int indexFirst = players.indexOf(firstPlayer);
+        ArrayList<String> playerInOrder = new ArrayList<>();
 
+        //Order player according with the chair
+        if(indexFirst !=0) {
+            for (int i = indexFirst; i < numberOfPlayers; i++) {
+                playerInOrder.add(players.get(i));
+            }
+            for(int i = 0;i<indexFirst;i++){
+                playerInOrder.add(players.get(i));
+            }
+        }
+        else {
+            for(int i = 0;i<numberOfPlayers;i++){
+                playerInOrder.add(players.get(i));
+            }
+        }
+
+        this.turnController = new TurnController(playerInOrder,match.getChair().getNickname(),match);
         //virtualview per far vedere le personal card e manda un messagio al primo giocatore e farlo iniziare.
+    }
+
+    /**
+     * next player
+     * call endgame when don't have a next player
+     */
+    private void nextPlayer(){
+        if(turnController.nextPlayer() == true);
+        //TODO avviso il player che deve giocare
+        else{
+            endGame();
+
+        }
+    }
+
+    /**
+     * END GAME
+     * Send the score to the player
+     */
+    public void endGame(){
+        HashMap<String,Integer> results = match.getMatchmanager().results(match);
+        //TODO avviso i giocatori che il gioco è finito e visualizza il punteggio.
+
+    }
+    private void firstFinish(Player p){
+        turnController.setLastRound(true);
+        match.setFirstFinish(p);
     }
 
     //------------------------On message received-------------------------------------------
@@ -105,12 +159,15 @@ public class MatchController {
      * @param m message
      */
     public void messageHandler (Message m){
-        if(turnController.getActivePlayer().equals(m.getnickname())){
+        if(isStarted == true) {
+            if (turnController.getActivePlayer().equals(m.getnickname())) {
+                m.visit(this);
+            } else {
+                // TODO virtualview avvisa al cliente che non è il suo turno.
+            }
+        }
+        else
             m.visit(this);
-        }
-        else{
-            // TODO virtualview avvisa al cliente che non è il sup turno.
-        }
     }
 
     /**
@@ -119,6 +176,8 @@ public class MatchController {
      */
     public void handler(Numb_Player numberPlayer){
         match.setMatch(numberPlayer.getNumb());
+        numberOfPlayers = numberPlayer.getNumb();
+
 
         // TODO virtualview che dice che attende gli altri giocatori
     }
@@ -132,12 +191,13 @@ public class MatchController {
             for(Card card : cardSelect){
                 match.getBoard().takeCard(card.getCoordinates());
             }
+            match.getMatchmanager().IsEmptyBoard(match);
 
             Player player = match.getPlayerByNickname(m.getnickname());
 
             int[] coloum = match.getPlayerByNickname(m.getnickname()).getLibrary().showColumn(cardSelect.size());
             //TODO messaggio virtualview per dire al giocatore le colonne possibili
-            //messaggio virtualview per dire al giocatore le colonne possibili
+
         }
         else{
             //messagio virtualview non può prenderli
@@ -147,6 +207,7 @@ public class MatchController {
 
     /**
      * Put the card in the library and notify the common cards
+     * call next player
      * @param m
      */
     public void handler(PutInLib m){
@@ -155,9 +216,13 @@ public class MatchController {
         ArrayList<Card> cards = m.getCardsInOrder();
 
         match.getPlayerByNickname(player).getLibrary().setColumn(cards,coloum);
+        if(match.getPlayerByNickname(player).getLibrary().isFull())
+            firstFinish(match.getPlayerByNickname(player));
         match.getPlayerByNickname(player).getPlayerManager().notifyAllObservers(match.getPlayerByNickname(player));
 
-        //TODO turncontroller cambia turno.
+
+        nextPlayer();
+
     }
 
     //----------------------VIRTUALVIEW METHODS----------------
