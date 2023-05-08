222087673ad2bbd3d4e9b438330179be383e4608
Silvia_Liu <silviaruxin.liu@mail.polimi.it>
2023-05-08 13:54:46 +0200

    socket connection

--- src/main/java/it/polimi/ingsw/Controller/ClientController.java
+++ src/main/java/it/polimi/ingsw/Controller/ClientController.java
@@ -23,7 +23,7 @@ public class ClientController implements ObserverViewClient {
     public ClientController(View view , VirtualModel virtualModel) throws IOException {
         this.view = view;
         this.virtualModel=virtualModel;
-        socketClient= new SocketClient("localhost" , 16000847);
+        socketClient= new SocketClient("localhost" , 16847);
     }
 
     public ClientController(View view , VirtualModel virtualModel , SocketClient socketClient){
