df786f00247ed6641b5e13c39eb6123eca05ac89
LucaLaPietra <111192833+LucaLaPietra@users.noreply.github.com>
2023-05-12 12:11:20 +0200

    First javaFxScene(Login)+login checker

--- src/main/java/it/polimi/ingsw/view/GUI/JavaFXApplication.java
+++ src/main/java/it/polimi/ingsw/view/GUI/JavaFXApplication.java
@@ -3,7 +3,9 @@ package it.polimi.ingsw.view.GUI;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 import java.util.Objects;
@@ -11,8 +13,10 @@ import java.util.Objects;
 public class JavaFXApplication extends Application {
  @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent p=FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxml/main_scene.fxml")));
-
+        Parent root=FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxml/main_scene.fxml")));
+        Scene scene=new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     public static void main(String[] args) {
         launch(args);
---
+++ src/main/java/it/polimi/ingsw/view/GUI/Scenes/Controller.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.view.GUI.Scenes;
+
+
+public interface Controller {
+}
