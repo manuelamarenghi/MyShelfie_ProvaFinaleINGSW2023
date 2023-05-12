f9bb2ad6a23878e99944d2d39781a0bf29e4439d
LucaLaPietra <111192833+LucaLaPietra@users.noreply.github.com>
2023-05-12 10:56:15 +0200

    working on javafx scene

--- src/main/java/it/polimi/ingsw/view/GUI/JavaFXApplication.java
+++ src/main/java/it/polimi/ingsw/view/GUI/JavaFXApplication.java
@@ -1,16 +1,20 @@
 package it.polimi.ingsw.view.GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
-public class JavaFXApplication extends Application {
+import java.io.IOException;
+import java.util.Objects;
 
+public class JavaFXApplication extends Application {
+ @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent p=FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxml/main_scene.fxml")));
+
+    }
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
     }
 }
