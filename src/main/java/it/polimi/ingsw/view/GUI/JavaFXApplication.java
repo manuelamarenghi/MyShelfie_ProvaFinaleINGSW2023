3283ff869bbeb2723ea37c47fb12b673cd9d837f
Singh10726934 <111194189+Singh10726934@users.noreply.github.com>
2023-05-16 15:49:42 +0200

    Board method allow updated (#122)
    Co-authored-by: lovishmultani <sukhnandan.singhmail@polimi.it>

--- src/main/java/it/polimi/ingsw/view/GUI/JavaFXApplication.java
+++ src/main/java/it/polimi/ingsw/view/GUI/JavaFXApplication.java
@@ -2,9 +2,17 @@ package it.polimi.ingsw.view.GUI;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class JavaFXApplication extends Application {
-
+    Button button;
     public static void main(String[] args) {
         launch(args);
     }
