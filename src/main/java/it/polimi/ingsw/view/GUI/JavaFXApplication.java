8527a30c464937d16a72d083a84a9ed87575b849
LucaLaPietra <111192833+LucaLaPietra@users.noreply.github.com>
2023-05-17 16:01:09 +0200

    Finished SceneController and started the logic of scenecontrolers

--- src/main/java/it/polimi/ingsw/view/GUI/JavaFXApplication.java
+++ src/main/java/it/polimi/ingsw/view/GUI/JavaFXApplication.java
@@ -32,9 +32,4 @@ public class JavaFXApplication extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }
