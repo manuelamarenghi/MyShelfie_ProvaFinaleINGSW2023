cccccb03e667298056fd80831b37fe2c706cdc2e
lovishmultani <sukhnandan.singhmail@polimi.it>
2023-05-17 15:36:42 +0200

    set text in CC1Controller

---
+++ src/main/java/it/polimi/ingsw/view/GUI/Scenes/CC2Controller.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.view.GUI.Scenes;
+
+import it.polimi.ingsw.modello.CommonGoalCard.Col2G;
+
+public class CC2Controller {
+    public Col2G commonGoalCard;
+    public void getDescription(){
+        commonGoalCard.getDesc();
+    }
+
+}
