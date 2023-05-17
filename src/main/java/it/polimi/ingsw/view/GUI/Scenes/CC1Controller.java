e28ec396b329ad01efc6b0614a6dd3cd34f79aea
lovishmultani <sukhnandan.singhmail@polimi.it>
2023-05-17 16:13:58 +0200

    set text in CC1Controller

--- src/main/java/it/polimi/ingsw/view/GUI/Scenes/CC1Controller.java
+++ src/main/java/it/polimi/ingsw/view/GUI/Scenes/CC1Controller.java
@@ -1,18 +1,22 @@
 package it.polimi.ingsw.view.GUI.Scenes;
 
 import it.polimi.ingsw.modello.CommonGoalCard.Square2G;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class CC1Controller implements Controller{
+        public Button theButton;
+        public Label theLabel;
         private Square2G commonGoalCard;
-        private Label CC1Label;
         private String textToShow;
         //
         public void getDescription(ActionEvent event) {
                 textToShow=commonGoalCard.getDesc();
-                CC1Label.setText(textToShow);
+                theLabel.setText(textToShow);
 
         }
 }
