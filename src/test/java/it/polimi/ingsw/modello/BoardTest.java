2381c9b5e5786744b806396e11453a08c691f55f
lovishmultani <sukhnandan.singhmail@polimi.it>
2023-05-17 14:42:10 +0200

    bug fixed in allow method in board

--- src/test/java/it/polimi/ingsw/modello/BoardTest.java
+++ src/test/java/it/polimi/ingsw/modello/BoardTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
     @Test
