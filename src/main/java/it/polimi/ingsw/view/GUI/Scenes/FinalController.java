92c2713b55e2f2cfbdfb267d342d6192a67d8eed
manuela <manuelamarenghi@mail.polimi.it>
2023-05-25 11:51:49 +0200

    rifinendo gui

--- src/main/java/it/polimi/ingsw/view/GUI/Scenes/FinalController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/Scenes/FinalController.java
@@ -8,8 +8,10 @@ import javafx.scene.text.Text;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FinalController extends ObservableViewClient implements GenericSceneController {
     @FXML
@@ -28,26 +30,17 @@ public class FinalController extends ObservableViewClient implements GenericScen
         ScoreTwo.toFront();
         ScoreThird.toFront();
         FourScore.toFront();
-        HashMap<String, Integer> con = new HashMap<>();
-        con.put("chiara", 3);
-        con.put("maria", 10);
-        con.put("carlo", 6);
-        SetClassification(con);
     }
     public void SetClassification(HashMap<String, Integer> point) {
-        ArrayList<Integer> sortedList = new ArrayList<>();
+        ArrayList<Integer> List = new ArrayList<>();
         for (int x : point.values()) {
-            sortedList.add(x);
+            List.add(x);
         }
-        sortedList.stream().sorted();
-        System.out.println(sortedList);
+        ArrayList<Integer> sortedList = (ArrayList<Integer>) List.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         Set<String> key = point.keySet();
-        System.out.println(key);
         int i = 0;
         for (String s : key) {
             while (i < sortedList.size()) {
-                System.out.println(s);
-                System.out.println(sortedList.get(i));
                 if (point.get(s).equals(sortedList.get(i))) {
                     InputStream is;
                     switch (i) {
@@ -56,40 +49,38 @@ public class FinalController extends ObservableViewClient implements GenericScen
                             Image image = new Image(is);
                             FIrstStar.setImage(image);
                             FirstPlace.setText(s);
-                            ScoreOne = new Text();
                             ScoreOne.setText(sortedList.get(i).toString());
+                            i = 5;
                             break;
                         case 1:
                             is = this.getClass().getResourceAsStream("/images/Publisher_material/stellina.png");
                             Image imageo = new Image(is);
                             SecStar.setImage(imageo);
-                            SecondPlace = new Text();
                             SecondPlace.setText(s);
-                            ScoreTwo = new Text();
                             ScoreTwo.setText(sortedList.get(i).toString());
+                            i = 5;
                             break;
                         case 2:
                             is = this.getClass().getResourceAsStream("/images/Publisher_material/stellina.png");
                             Image imagea = new Image(is);
                             Third.setImage(imagea);
-                            ThirdPlace = new Text();
                             ThirdPlace.setText(s);
-                            ScoreThird = new Text();
                             ScoreThird.setText(sortedList.get(i).toString());
+                            i = 5;
                             break;
                         case 3:
                             is = this.getClass().getResourceAsStream("/images/Publisher_material/stellina.png");
                             Image imageu = new Image(is);
                             Four.setImage(imageu);
-                            FourPlace = new Text();
                             FourPlace.setText(s);
-                            FourScore = new Text();
                             FourScore.setText(sortedList.get(i).toString());
+                            i = 5;
                             break;
                     }
-                    i++;
                 }
+                i++;
             }
+            i = 0;
         }
     }
 
